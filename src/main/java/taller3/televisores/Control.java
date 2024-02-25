60ae83f49377927b6afb04054051bfee3fcd058a
Andrés Pérez Cano <aperezcan@unal.edu.co>
2024-02-24 20:21:37 -0500

    correccion 4

--- src/main/java/taller3/televisores/Control.java
+++ src/main/java/taller3/televisores/Control.java
@@ -4,32 +4,50 @@ public class Control {
     private TV tv;
 	
 	public void turnOn() {
-		tv.turnOn();	
+		if (tv != null) {
+			tv.turnOn();
+		}
 	}
 	public void turnOff() {
-		tv.turnOff();
+		if(tv != null) {
+			tv.turnOff();
+		}
 	}
 	public void canalUp() {
-		tv.canalUp();
+		if(tv != null) {
+			tv.canalUp();
+		}
 	}
 	public void canalDown() {
-		tv.canalDown();
+		if(tv != null) {
+		    tv.canalDown();
+		}
 	}
 	public void volumenUp() {
-		tv.volumenUp();
+		if(tv != null) {
+		    tv.volumenUp();
+		}
 	}
     public void volumenDown() {
-    	tv.volumenDown();
+    	if(tv != null) {
+    	    tv.volumenDown();
+    	}
     }
     public void setCanal(int canal) {
-    	tv.setCanal(canal);
+    	if(tv != null) {
+    	    tv.setCanal(canal);
+    	}
     }
     public void setVolumen(int volumen) {
-    	tv.setVolumen(volumen);
+    	if(tv != null) {
+    	    tv.setVolumen(volumen);
+    	}
     }
     public void enlazar(TV tv) {
 		this.tv = tv;
-		tv.setControl(this);
+		if(tv != null) {
+		     tv.setControl(this);
+		}
 	}
 	public void setTv(TV tv) {
 		this.tv = tv;
